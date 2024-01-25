ec461c1ec0b8ddd3b109e6b4fc9fcc92dff9a045
Alan Machado Ferreira <alanmferreira@hotmail.com>
2024-01-24 21:34:30 -0300

    CartWidget ajustes

--- src/components/Cartwidget/index.jsx
+++ src/components/Cartwidget/index.jsx
@@ -2,7 +2,7 @@ import FeatherIcon from 'feather-icons-react';
 
 export default function Cartwidget () {
     return (
-        <div className='hidden lg:flex '>
+        <div className='hidden lg:flex'>
           <FeatherIcon icon="shopping-bag" />
         </div>        
     )
